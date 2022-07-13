5772c29da85b5ea00a102a29d18ebf16a0d39344
Erik Ivan Retana Lopez <merzelert@gmail.com>
2022-07-12 19:34:52 -0500

    comentarios

--- script.js
+++ script.js
@@ -1,52 +1,26 @@
 //Codigo del cuadrado
 console.group("Cuadrados");
-/* const ladoCuadrado = 5; 
-console.log("Los lados del cuadrado miden " + ladoCuadrado + " cm");
- */
 function perimetroCuadrado (lado) {
 return lado * 4;
 }
 perimetroCuadrado();
-
-/* console.log("El perimetro del cuadrado es " + perimetroCuadrado + " cm");
- */
-
 function areaCuadrada (lado){
     return lado * lado;
 } 
 areaCuadrada();
-/* 
-console.log("El area del cuadrado es " + areaCuadrada + " cm^2");
- */
 console.groupEnd("Cuadrados");
 
 //Codigo del triangulo
 console.group("Triangulos");
-/* 
-const ladoTriangulo1= 6;
-const ladoTriangulo2= 6;
-const baseTriangulo= 4;
-const alturaTriangulo= 5.5;
- */
-/* console.log("Los lados del tringulo miden " 
-+ ladoTriangulo1 + " cm, "
-+ ladoTriangulo2 + " cm, "
-+ baseTriangulo + " cm ");
-console.log("La altura del triangulo es " + alturaTriangulo + " cm");
- */
 function perimetroTriangulo (lado1, lado2, base){
     return (Number(lado1))+(Number(lado2))+(Number(base));
 }
 perimetroTriangulo();
 
-/* console.log("El perimeto del triangulo es " + perimetroTriangulo + " cm");
- */
 function areaTriangulo (base, altura){
     return (base*altura)/2;
 } 
 areaTriangulo();
-/* console.log("El area del triangulo es " + areaTriangulo + " cm^2");
- */
 console.groupEnd("Triangulos");
 
 
@@ -56,9 +30,6 @@ console.groupEnd("Triangulos");
 console.group("Circulos");
 
 //Radio
-
-/* const radioCirculo = 4;
-console.log("El radio del circulo es " + radioCirculo + " cm");
  *///Diametro
 
 function diametroCirculo(radio){
@@ -78,17 +49,10 @@ function perimetroCirculo(radio){
 }
 perimetroCirculo();
 
-
-/* console.log("El perimetro del circulo es " + perimetroCirculo + " cm");
- *///Area
-
 function areaCirculo(radio){
     return (radio*radio)*PI;
 } 
 
-
-/* console.log("El area del circulo es " + areaCirculo + " cm");
- */
 console.groupEnd("Circulos");
 
 //Aqui interactuamos con HTML
